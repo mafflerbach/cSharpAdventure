8858c58a4da1f22ad510f05c74abf6bdba57ac14
mafflerbach <maren@afflerbach.info>
2015-09-18 22:24:13 +0200

    initial Commit - First step ever in C#

---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WpfApplication1
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBox_OnInitialized(object sender, EventArgs e)
+        {
+            List<Gw.ItemList> items = new List<Gw.ItemList>();
+            items.Add(new Gw.ItemList() { Title = "Complete this WPF tutorial" , Image = "Images/Bandage.png" });
+            items.Add(new Gw.ItemList() { Title = "Learn C#", Image = "Images/Bandage.png" });
+            items.Add(new Gw.ItemList() { Title = "Wash the car", Image = "Images/Bandage.png" });
+
+            ListBox.ItemsSource = items;
+            
+        }
+    }
+
+
+}
