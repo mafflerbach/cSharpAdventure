e3cbac5ca1d2bb1d0e25e85b4f71bcb0748f6027
mafflerbach <maren@afflerbach.info>
2015-09-21 15:39:23 +0200

    small refactoring

--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -15,21 +15,17 @@ namespace WpfApplication1
     {
 
         private int page = 1;
+        private dynamic data;
         public MainWindow()
         {
             InitializeComponent();
-            Console.WriteLine("Main");
             tbCurrentPage.Text = "1";
             TbTotalPage.Text = "0";
-            // wg reihenfolge
-            //TbTotalPage.Text = pageSum;
-
         }
 
         private void ListBox_OnInitialized(object sender, EventArgs e)
         {
-            var data = this.getJsonRequest();
-
+            this.data = this.getJsonRequest();
             string pageSum = "";
             
             var items = new List<ItemList>();
@@ -39,45 +35,44 @@ namespace WpfApplication1
 
         }
 
+
+        private void ListBox_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            this.data = this.getJsonRequest();
+            TbTotalPage.Text = this.data.last_page;
+        }
+
+
         private void search_Click(object sender, RoutedEventArgs e)
         {
-            var searchTerm = SearchTermTextBox.Text;
-
-            Search foo = new Search();
-            foo.page = 1;
-            tbCurrentPage.Text = "1";
-            dynamic data = foo.Request(searchTerm);
-            TbTotalPage.Text = data.last_page.ToString();
-
-            var items = new List<ItemList>();
-            this.fillListBox(items, data);
-            ListBox.ItemsSource = items;
+ 
+            this.updateListBox(0);
         }
         
         private dynamic getJsonRequest(string querry = "*")
         {
             Search foo = new Search();
             foo.page = 1;
-            dynamic data =  foo.Request(querry);
+            this.data =  foo.Request(querry);
 
-            return data;
+            return this.data;
         }
 
         private void fillListBox(List<ItemList> items, dynamic data)
         {
-            for (var i = 0; i < data.results.Count; i++)
+            for (var i = 0; i < this.data.results.Count; i++)
             {
                 var webClient = new WebClient();
-                byte[] imageBytes = webClient.DownloadData(data.results[i].img.ToString());
+                byte[] imageBytes = webClient.DownloadData(this.data.results[i].img.ToString());
                 items.Add(new ItemList()
                 {
-                    Title = data.results[i].name.ToString(),
+                    Title = this.data.results[i].name.ToString(),
                     Image = imageBytes,
-                    Rarity = data.results[i].rarity.ToString(),
-                    restriction_level = data.results[i].restriction_level.ToString(),
-                    max_offer_unit_price = data.results[i].max_offer_unit_price.ToString(),
-                    min_sale_unit_price = data.results[i].min_sale_unit_price.ToString(),
-                    sale_availability = data.results[i].sale_availability.ToString(),
+                    Rarity = this.data.results[i].rarity.ToString(),
+                    restriction_level = this.data.results[i].restriction_level.ToString(),
+                    max_offer_unit_price = this.data.results[i].max_offer_unit_price.ToString(),
+                    min_sale_unit_price = this.data.results[i].min_sale_unit_price.ToString(),
+                    sale_availability = this.data.results[i].sale_availability.ToString(),
                 });
 
             }
@@ -85,32 +80,15 @@ namespace WpfApplication1
 
         private void next_Click(object sender, RoutedEventArgs e)
         {
-            
-            var searchTerm = SearchTermTextBox.Text;
-            if (searchTerm.Equals(""))
-            {
-                searchTerm = "*";
-            }
-            
-            Search foo = new Search();
-
-            //todo Convert.ToInt32(data.last_page)
-            if (Convert.ToInt32(tbCurrentPage.Text) < 10)
-            {
-                foo.page = Convert.ToInt32(tbCurrentPage.Text) + 1;
-            }
-            else
-            {
-                foo.page = 1;
-            }
-            dynamic data = foo.Request(searchTerm);
-            tbCurrentPage.Text = foo.page.ToString();
-            TbTotalPage.Text = data.last_page.ToString();
-            var items = new List<ItemList>();
-            this.fillListBox(items, data);
-            ListBox.ItemsSource = items;
+            this.updateListBox(1);
+      
         }
         private void prev_Click(object sender, RoutedEventArgs e)
+        {
+            this.updateListBox(-1);
+        }
+
+        private void updateListBox(int calcPage)
         {
             var searchTerm = SearchTermTextBox.Text;
             if (searchTerm.Equals(""))
@@ -119,23 +97,37 @@ namespace WpfApplication1
             }
 
             Search foo = new Search();
+            foo.page = getPage(calcPage);
 
-            if (Convert.ToInt32(tbCurrentPage.Text) > 1)
+            this.data = foo.Request(searchTerm);
+            tbCurrentPage.Text = foo.page.ToString();
+            TbTotalPage.Text = this.data.last_page;
+
+            var items = new List<ItemList>();
+            this.fillListBox(items, this.data);
+            ListBox.ItemsSource = items;
+
+        }
+        
+
+        private int getPage(int calcPage)
+        {
+
+            int maxPage = Convert.ToInt32(TbTotalPage.Text);
+            int currentPage = Convert.ToInt32(tbCurrentPage.Text);
+            int page = 0;
+            
+            if (currentPage > 1 || currentPage < maxPage)
             {
-                foo.page = Convert.ToInt32(tbCurrentPage.Text) - 1;
+                page = currentPage + calcPage;
             }
             else
             {
-                foo.page = 1;
+                page = 1;
             }
-
-            dynamic data = foo.Request(searchTerm);
-            tbCurrentPage.Text = foo.page.ToString();
-            TbTotalPage.Text = data.last_page.ToString();
-
-            var items = new List<ItemList>();
-            this.fillListBox(items, data);
-            ListBox.ItemsSource = items;
+   
+            return page;
         }
+
     }
 }
