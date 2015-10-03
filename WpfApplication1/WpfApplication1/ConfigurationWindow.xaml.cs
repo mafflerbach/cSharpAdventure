312107287e8c1910fc841b40d07ff26223f1d695
mafflerbach <maren@afflerbach.info>
2015-10-03 22:58:34 +0200

    rafactoring, add settings window

---
+++ WpfApplication1/WpfApplication1/ConfigurationWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication1 {
+  /// <summary>
+  /// Interaction logic for ConfigurationWindow.xaml
+  /// </summary>
+  public partial class ConfigurationWindow : Window {
+    public ConfigurationWindow() {
+      InitializeComponent();
+    }
+
+    private void cancel_Click(object sender, RoutedEventArgs e) {
+      Close();
+    }
+
+    private void save_Click(object sender, RoutedEventArgs e) {
+      Properties.Settings.Default.API_KEY = ApiTextBox.Text;
+      Console.WriteLine(Properties.Settings.Default.API_KEY);
+      Close();
+    }
+  }
+}
--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Windows;
 using Api.Item;
 using WpfApplication1.Gw;
@@ -15,9 +16,8 @@ namespace WpfApplication1 {
     private int page = 1;
 
     public MainWindow() {
-
       CachedImage.FileCache.AppCacheDirectory = string.Format("{0}\\Images2\\",
-                              Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
       InitializeComponent();
       tbCurrentPage.Text = "1";
       TbTotalPage.Text = "0";
@@ -26,7 +26,7 @@ namespace WpfApplication1 {
     private void ListBox_OnInitialized(object sender, EventArgs e) {
       data = getSearchResult(1);
       var pageSum = "";
-      
+
       var items = new List<ItemList>();
       ListBoxProvider provider = new ListBoxProvider(items, data);
       provider.fillListBox();
@@ -40,7 +40,7 @@ namespace WpfApplication1 {
       var items1 = new List<ItemList>();
       ListBoxProvider provider = new ListBoxProvider(items1, data);
       provider.fillListBox();
-      
+
       ListBox1.ItemsSource = items1;
     }
 
@@ -51,7 +51,7 @@ namespace WpfApplication1 {
       var items2 = new List<ItemList>();
       ListBoxProvider provider = new ListBoxProvider(items2, data);
       provider.fillListBox();
-      
+
       ListBox2.ItemsSource = items2;
     }
 
@@ -66,17 +66,8 @@ namespace WpfApplication1 {
       data = getSearchResult(1);
       updateListBox(data);
     }
-
-    private dynamic getJsonRequest(string querry = "*") {
-      var foo = new Search();
-      foo.page = 1;
-      data = foo.Request(querry);
-
-      return data;
-    }
-
+    
     private void next_Click(object sender, RoutedEventArgs e) {
-
       data = getSearchResult(1);
       updateListBox(data);
     }
@@ -88,7 +79,6 @@ namespace WpfApplication1 {
 
 
     private void updateListBox(dynamic data) {
-      
       var items = new List<ItemList>();
       ListBoxProvider provider = new ListBoxProvider(items, data);
       provider.fillListBox();
@@ -98,13 +88,11 @@ namespace WpfApplication1 {
 
 
     private dynamic getSearchResult(int calcPage) {
-
       var searchTerm = "*";
       if (SearchTermTextBox != null && !SearchTermTextBox.Text.Equals("")) {
         searchTerm = SearchTermTextBox.Text;
       }
       
-
       var search = new Search();
       search.page = getPage(calcPage);
       data = search.Request(searchTerm);
@@ -118,65 +106,52 @@ namespace WpfApplication1 {
       }
       return data;
     }
-    
+
 
     private int getPage(int calcPage) {
       int maxPage;
       int currentPage;
-
-
-      if (TbTotalPage != null &&  !TbTotalPage.Text.Equals("")) {
+      
+      if (TbTotalPage != null && !TbTotalPage.Text.Equals("")) {
         maxPage = Convert.ToInt32(TbTotalPage.Text);
       }
       else {
         maxPage = 0;
-      };
+      }
+
       if (tbCurrentPage != null && !tbCurrentPage.Text.Equals("")) {
         currentPage = Convert.ToInt32(tbCurrentPage.Text);
       }
       else {
         currentPage = 0;
       }
-      
+
       var page = 0;
 
-      
-      
-      if (currentPage > 1 && currentPage < maxPage) {
+      if (currentPage >= 1 && currentPage < maxPage) {
         page = currentPage + calcPage;
       }
       else {
         page = 1;
       }
-      
+
       return page;
     }
 
 
     private void goTo_Click(object sender, RoutedEventArgs e) {
+      data = getSearchResult(0);
+      updateListBox(data);
 
-      var currentPage = Convert.ToInt32(tbCurrentPage.Text);
-      var foo = new Search();
-      foo.page = currentPage;
-      var searchTerm = SearchTermTextBox.Text;
-      if (searchTerm.Equals(""))
-      {
-        searchTerm = "*";
-      }
-      data = foo.Request(searchTerm);
-      tbCurrentPage.Text = currentPage.ToString();
-      TbTotalPage.Text = data.last_page;
-
-      var items = new List<ItemList>();
-      
-      ListBoxProvider provider = new ListBoxProvider(items, data);
-      provider.fillListBox();
-      ListBox.ItemsSource = items;
-
-
-      ListBox.ItemsSource = items;
       ListBox.ScrollIntoView(ListBox.Items[0]);
-
+    }
+    
+    private void MenuItem_Settings(object sender, RoutedEventArgs e) {
+      var settings = new ConfigurationWindow();
+      settings.Show();
+    }
+    private void MenuItem_Close(object sender, RoutedEventArgs e) {
+      Application.Current.Shutdown();
     }
   }
 }
